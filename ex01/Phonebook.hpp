2cc1137dae6204c7f9aee2a03b385d3438c5c39c
Hushy <99977761+Hushhhy@users.noreply.github.com>
2025-01-08 19:05:25 +0000

    ex00 + start ex01

---
+++ ex01/Phonebook.hpp
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Phonebook.hpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: codespace <codespace@student.42.fr>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/01/08 18:54:33 by codespace         #+#    #+#             */
+/*   Updated: 2025/01/08 19:01:18 by codespace        ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PHONEBOOK_HPP
+# define PHONEBOOK_HPP
+
+# include "Contact.hpp"
+# include <iostream>
+# include <limits>
+
+
+
+#endif
